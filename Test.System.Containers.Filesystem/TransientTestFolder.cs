26f2e3ba2445c8487f581c4c6e5177344cb3d284
Rainer Sigwald <raines@microsoft.com>
2022-06-02 16:26:14 -0500

    TransientTestFolder

---
+++ Test.System.Containers.Filesystem/TransientTestFolder.cs
@@ -0,0 +1,22 @@
+﻿using static System.IO.Path;
+
+namespace Test.System.Containers.Filesystem;
+
+/// <summary>
+/// Helper class to clean up after tests that touch the filesystem.
+/// </summary>
+internal sealed class TransientTestFolder : IDisposable
+{
+    public readonly string Path = Combine(GetTempPath(), GetRandomFileName());
+    public readonly DirectoryInfo DirectoryInfo;
+
+    public TransientTestFolder()
+    {
+        DirectoryInfo = Directory.CreateDirectory(Path);
+    }
+
+    public void Dispose()
+    {
+        Directory.Delete(Path, recursive: true);
+    }
+}
