b3edd932b6f12f94bd75fa132ff606b80e9b1851
Rainer Sigwald <raines@microsoft.com>
2022-06-02 10:57:05 -0500

    Introduce Descriptor

---
+++ Test.System.Containers/DescriptorTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Containers;
+using System.Text.Json;
+
+namespace Test.System.Containers;
+
+[TestClass]
+public class DescriptorTests
+{
+    [TestMethod]
+    public void BasicConstructor()
+    {
+        Descriptor d = new(
+            mediaType: "application/vnd.oci.image.manifest.v1+json",
+            digest: "sha256:5b0bcabd1ed22e9fb1310cf6c2dec7cdef19f0ad69efa1f392e94a4333501270",
+            size: 7682);
+
+        Console.WriteLine(JsonSerializer.Serialize(d, new JsonSerializerOptions { WriteIndented = true }));
+
+        Assert.AreEqual("application/vnd.oci.image.manifest.v1+json", d.MediaType);
+        Assert.AreEqual("sha256:5b0bcabd1ed22e9fb1310cf6c2dec7cdef19f0ad69efa1f392e94a4333501270", d.Digest);
+        Assert.AreEqual(7_682, d.Size);
+
+        Assert.IsNull(d.Annotations);
+        Assert.IsNull(d.Data);
+        Assert.IsNull(d.Urls);
+    }
+}
