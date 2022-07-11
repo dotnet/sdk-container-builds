ca11a947681a0f3a36b475cebb744068065db8eb
Ben Villalobos <4691428+BenVillalobos@users.noreply.github.com>
2022-07-11 13:28:16 -0700

    Add System.Containers.Tasks

---
+++ System.Containers.Tasks/CreateNewImage.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+#nullable disable
+
+namespace System.Containers.Tasks
+{
+    public class CreateNewImage : Microsoft.Build.Utilities.Task
+    {
+        /// <summary>
+        /// Base image name.
+        /// </summary>
+        [Required]
+        public string BaseImageName { get; set; }
+
+        [Required]
+        public string BaseImageTag { get; set; }
+
+        [Required]
+        public string InputRegistryURL { get; set; }
+
+        [Required]
+        public string OutputRegistryURL { get; set; }
+
+        [Required]
+        public ITaskItem[] Files { get; set; }
+
+        /// <summary>
+        /// $(ContainerWorkingDirectory)
+        /// </summary>
+        [Required]
+        public string WorkingDirectory { get; set; }
+
+        [Required]
+        public string NewImageName { get; set; }
+
+        [Required]
+        public string Entrypoint { get; set; }
+
+        /// <summary>
+        /// Arguments to pass alongside Entrypoint.
+        /// </summary>
+        public string EntrypointArgs { get; set; }
+
+        public string PublishDirectory { get; set; }
+
+        /// <summary>
+        /// CreateNewImage needs to:
+        /// 1. Pull a base image (needs parameters: URL, BaseImage, BaseImageTag)
+        /// 2. Add output of build as a new layer
+        /// 3. Push image back to some registry (needs parameters: OutputURL, NewName, EntryPoint)
+        /// </summary>
+        /// <returns></returns>
+        public override bool Execute()
+        {
+            if (Files.Length == 0)
+            {
+                Console.WriteLine("Files is empty, aborting.");
+                return false;
+            }
+
+            Registry reg = new Registry(new Uri(InputRegistryURL));
+
+            Image image;
+            try
+            {
+                image = reg.GetImageManifest(BaseImageName, BaseImageTag).Result;
+            }
+            catch
+            {
+                Console.WriteLine("GetImageManifest failed");
+                return false;
+            }
+
+            // Turn the build output (as items) into an array of filepaths
+            string[] filePaths = Files.Select((f) => f.ItemSpec).ToArray();
+
+            Layer newLayer = Layer.FromDirectory(Path.GetDirectoryName(filePaths[0]), WorkingDirectory);
+
+            image.AddLayer(newLayer);
+            image.SetEntrypoint(Entrypoint, EntrypointArgs?.Split(' ').ToArray());
+
+            Registry outputReg = new Registry(new Uri(OutputRegistryURL));
+
+            try
+            {
+                outputReg.Push(image, NewImageName).Wait();
+            }
+            catch
+            {
+                Console.WriteLine("Registry.Push failed");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Test.System.Containers/CreateNewImageTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Containers.Tasks;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.System.Containers
+{
+    [TestClass]
+    public class CreateNewImageTests
+    {
+        [TestMethod]
+        public void BasicCall()
+        {
+            CreateNewImage task = new CreateNewImage();
+            task.BaseImageName = "dotnet/runtime";
+            task.BaseImageTag = "6.0";
+            task.InputRegistryURL = "https://localhost:5000";
+            task.OutputRegistryURL = "https://localhost:5000";
+
+            ITaskItem[] files = new ITaskItem[1];
+            files[0] = new TaskItem("foo.bar");
+
+            task.Files = files;
+            task.WorkingDirectory = "app/";
+            task.NewImageName = "dotnet/newapp";
+            task.Entrypoint = "dotnet newapp.dll";
+
+            task.Execute();
+        }
+    }
+}
