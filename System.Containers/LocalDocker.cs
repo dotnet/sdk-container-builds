1ef7c765b87c909ac83cb16d08da608c315992cc
Rainer Sigwald <raines@microsoft.com>
2022-07-28 10:30:28 -0500

    Start LocalDocker.Load

---
+++ System.Containers/LocalDocker.cs
@@ -0,0 +1,21 @@
+﻿namespace System.Containers;
+
+internal class LocalDocker
+{
+    public async Task Load(Image x, string name, string baseName)
+    {
+        // Populate local cache with all layer tarballs
+
+        // Create new stream tarball
+
+        // Feed each layer tarball into the stream
+
+        // Add manifest
+
+        // add config
+
+        // call `docker load`
+
+        // give it a tag?
+    }
+}
