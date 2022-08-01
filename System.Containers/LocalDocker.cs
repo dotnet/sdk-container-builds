178c3bb0a454687a54f6041f0331d0400fabd0c8
Rainer Sigwald <raines@microsoft.com>
2022-08-01 13:25:55 -0500

    LocalImage works for existing layers (but not added ones yet)

--- System.Containers/LocalDocker.cs
+++ System.Containers/LocalDocker.cs
@@ -1,8 +1,8 @@
-﻿using System.Diagnostics;
+using System.Diagnostics;
 using System.Formats.Tar;
-using System.IO.Compression;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Nodes;
 
 namespace System.Containers;
 
@@ -39,6 +39,7 @@ public class LocalDocker
 
 
         // Feed each layer tarball into the stream
+        JsonArray layerTarballPaths = new JsonArray();
 
         foreach (var layerJson in x.manifest["layers"].AsArray())
         {
@@ -52,13 +53,17 @@ public class LocalDocker
             string localPath = await x.originatingRegistry.Value.LocalFileForBlob(baseName, d);
 
             // Stuff that (uncompressed) tarball into the image tar stream
-            writer.WriteEntry(localPath, $"{d.Digest.Substring("sha256:".Length)}/layer.tar");
+            string layerTarballPath = $"{d.Digest.Substring("sha256:".Length)}/layer.tar";
+            writer.WriteEntry(localPath, layerTarballPath);
+            layerTarballPaths.Add(layerTarballPath);
         }
 
         // add config
+        string configTarballPath = $"{Image.GetSha(x.config)}.json";
+
         using (MemoryStream configStream = new MemoryStream(Encoding.UTF8.GetBytes(x.config.ToJsonString())))
         {
-            GnuTarEntry configEntry = new(TarEntryType.RegularFile, $"{Image.GetSha(x.config)}.json")
+            GnuTarEntry configEntry = new(TarEntryType.RegularFile, configTarballPath)
             {
                 DataStream = configStream
             };
@@ -67,5 +72,26 @@ public class LocalDocker
         }
 
         // Add manifest
+        JsonArray tagsNode = new()
+        {
+            name + ":latest" // TODO: do something else here?
+        };
+
+        JsonNode manifestNode = new JsonArray(new JsonObject
+        {
+            { "Config", configTarballPath },
+            { "RepoTags", tagsNode },
+            { "Layers", layerTarballPaths }
+        });
+
+        using (MemoryStream manifestStream = new MemoryStream(Encoding.UTF8.GetBytes(manifestNode.ToJsonString())))
+        {
+            GnuTarEntry manifestEntry = new(TarEntryType.RegularFile, "manifest.json")
+            {
+                DataStream = manifestStream
+            };
+
+            writer.WriteEntry(manifestEntry); // TODO: asyncify these when API available (Preview 7)
+        }
     }
 }
