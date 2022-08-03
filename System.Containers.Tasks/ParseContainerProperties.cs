878772d0d5c2ce266422e7f446ab1c634a8ede6c
Ben Villalobos <4691428+BenVillalobos@users.noreply.github.com>
2022-08-03 10:35:36 -0700

    Add ParseContainerProperties & Tests

---
+++ System.Containers.Tasks/ParseContainerProperties.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System.Containers;
+
+#nullable disable
+
+namespace System.Containers.Tasks
+{
+    public class ParseContainerProperties : Microsoft.Build.Utilities.Task
+    {
+
+
+        /// <summary>
+        /// The full base image name. mcr.microsoft.com/dotnet/runtime:6.0, for example.
+        /// </summary>
+        [Required]
+        public string FullyQualifiedBaseImageName { get; set; }
+
+        /// <summary>
+        /// The image name for the container to be created.
+        /// </summary>
+        [Required]
+        public string ContainerImageName { get; set; }
+
+        /// <summary>
+        /// The tag for the container to be created.
+        /// </summary>
+        [Required]
+        public string ContainerImageTag { get; set; }
+
+        [Output]
+        public string ParsedContainerRegistry { get; private set; }
+
+        [Output]
+        public string ParsedContainerImage { get; private set; }
+
+        [Output]
+        public string ParsedContainerTag { get; private set; }
+
+        [Output]
+        public string NewContainerImageName { get; private set; }
+
+        [Output]
+        public string NewContainerTag { get; private set; }
+
+        public override bool Execute()
+        {
+            if (!ContainerHelpers.IsValidImageName(ContainerImageName))
+            {
+                Log.LogError("Invalid image name: {0}", ContainerImageName);
+                return !Log.HasLoggedErrors;
+            }
+
+            if (!ContainerHelpers.IsValidImageTag(ContainerImageTag))
+            {
+                Log.LogError("Invalid image tag: {0}", ContainerImageTag);
+                return !Log.HasLoggedErrors;
+            }
+
+            if (!ContainerHelpers.TryParseFullyQualifiedContainerName(FullyQualifiedBaseImageName,
+                                                                      out string outputReg,
+                                                                      out string outputImage,
+                                                                      out string outputTag))
+            {
+                Log.LogError("Could not parse the given ContainerBaseImage: {0}", FullyQualifiedBaseImageName);
+                return !Log.HasLoggedErrors;
+            }
+
+            ParsedContainerRegistry = outputReg;
+            ParsedContainerImage = outputImage;
+            ParsedContainerTag = outputTag;
+            NewContainerImageName = ContainerImageName;
+            NewContainerTag = ContainerImageTag;
+
+            if (BuildEngine != null)
+            {
+                Log.LogMessage("Parsed the following properties. Note: Spaces are replaced with dashes.");
+                Log.LogMessage("Host: {0}", ParsedContainerRegistry);
+                Log.LogMessage("Image: {0}", ParsedContainerImage);
+                Log.LogMessage("Tag: {0}", ParsedContainerTag);
+                Log.LogMessage("Image Name: {0}", NewContainerImageName);
+                Log.LogMessage("Image Tag: {0}", NewContainerTag);
+            }
+
+            return !Log.HasLoggedErrors;
+        }
+    }
+}
