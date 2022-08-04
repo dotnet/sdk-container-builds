c4619ef654bcbe97de0b8fe075339211e9950cb3
Ben Villalobos <4691428+BenVillalobos@users.noreply.github.com>
2022-08-04 11:35:52 -0700

    PR cleanup, validation uses regex, tests updated

--- System.Containers/ContainerHelpers.cs
+++ System.Containers/ContainerHelpers.cs
@@ -1,97 +1,92 @@
-namespace System.Containers
+namespace System.Containers;
+
+using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+
+public static class ContainerHelpers
 {
-    public static class ContainerHelpers
+    private static string[] knownImageSeparators = { "-", ".", "_" };
+
+    private static Regex imageTagRegex = new Regex("[a-zA-Z0-9_][a-zA-Z0-9._-]{0,127}");
+
+    private static Regex imageNameRegex = new Regex("[a-z0-9]+([._-][a-z0-9]+)*(/[a-z0-9]+([._-][a-z0-9]+)*)*");
+
+    /// <summary>
+    /// Given some "fully qualified" image name (e.g. mcr.microsoft.com/dotnet/runtime), return
+    /// a valid UriBuilder. This means appending 'https' if the URI is not absolute, otherwise UriBuilder will throw.
+    /// </summary>
+    /// <param name="containerBase"></param>
+    /// <returns>A UriBuilder with the given containerBase, or, if containerBase is relative, https:// + containerBase</returns>
+    private static Uri? ContainerImageToUri(string containerBase)
     {
-        private static string[] knownImageSeparators = { "-", ".", "_" };
+        Uri uri = new Uri(containerBase, UriKind.RelativeOrAbsolute);
 
-        /// <summary>
-        /// Given some "fully qualified" image name (e.g. mcr.microsoft.com/dotnet/runtime), return
-        /// a valid UriBuilder. This means appending 'https' if the URI is not absolute, otherwise UriBuilder will throw.
-        /// </summary>
-        /// <param name="containerBase"></param>
-        /// <returns>A UriBuilder with the given containerBase, or, if containerBase is relative, https:// + containerBase</returns>
-        private static UriBuilder? ContainerImageToUriBuilder(string containerBase)
+        try
         {
-            Uri uri = new Uri(containerBase, UriKind.RelativeOrAbsolute);
+            return uri.IsAbsoluteUri ? uri : new Uri(containerBase.Contains("localhost") ? "http://" : "https://" + uri);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Failed parsing the container image into a UriBuilder: {0}", e);
+            return null;
+        }
+    }
 
-            try
-            {
-                if (uri.IsAbsoluteUri)
-                {
-                    return new UriBuilder(uri);
-                }
-                else
-                {
-                    // todo: allow customization?
-                    return new UriBuilder(containerBase.Contains("localhost") ? "http://" : "https://" + uri);
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Failed parsing the container image into a UriBuilder: {0}", e);
-                return null;
-            }
+    /// <summary>
+    /// Ensures the given image name is valid.
+    /// Spec: https://github.com/opencontainers/distribution-spec/blob/4ab4752c3b86a926d7e5da84de64cbbdcc18d313/spec.md#pulling-manifests
+    /// </summary>
+    /// <param name="imageName"></param>
+    /// <returns></returns>
+    public static bool IsValidImageName(string imageName)
+    {
+        return imageNameRegex.IsMatch(imageName);
+    }
+
+    /// <summary>
+    /// Ensures the given tag is valid.
+    /// Spec: https://github.com/opencontainers/distribution-spec/blob/4ab4752c3b86a926d7e5da84de64cbbdcc18d313/spec.md#pulling-manifests
+    /// </summary>
+    /// <param name="imageTag"></param>
+    /// <returns></returns>
+    public static bool IsValidImageTag(string imageTag)
+    {
+        return imageTag.Length <= 128 && imageTagRegex.IsMatch(imageTag);
+    }
+
+    /// <summary>
+    /// Parse a fully qualified container name (e.g. https://mcr.microsoft.com/dotnet/runtime:6.0)
+    /// Note: Tag not required.
+    /// </summary>
+    /// <param name="fullyQualifiedContainerName"></param>
+    /// <param name="containerRegistry"></param>
+    /// <param name="containerName"></param>
+    /// <param name="containerTag"></param>
+    /// <returns>True if the parse was successful. When false is returned, all out vars are set to empty strings.</returns>
+    public static bool TryParseFullyQualifiedContainerName(string fullyQualifiedContainerName, 
+                                                            [NotNullWhen(true)] out string? containerRegistry, 
+                                                            [NotNullWhen(true)] out string? containerName, 
+                                                            [NotNullWhen(true)] out string? containerTag)
+    {
+        Uri? uri = ContainerImageToUri(fullyQualifiedContainerName);
+
+        if (uri == null || uri.Segments.Length <= 1)
+        {
+            containerRegistry = null;
+            containerName = null;
+            containerTag = null;
+            return false;
         }
 
-        /// <summary>
-        /// Ensures the given image does not start or end with known separators, which are invalid.
-        /// </summary>
-        /// <param name="imageName"></param>
-        /// <returns></returns>
-        public static bool IsValidImageName(string imageName)
-        {
-            for (int i = 0; i < knownImageSeparators.Length; i++)
-            {
-                if (imageName.StartsWith(knownImageSeparators[i]) || imageName.EndsWith(knownImageSeparators[i]))
-                {
-                    return false;
-                }
-            }
+        // The first segment is the '/', create a string out of everything after.
+        string image = uri.PathAndQuery.Substring(1);
 
-            return true;
-        }
+        // If the image has a ':', there's a tag we need to parse.
+        int indexOfColon = image.IndexOf(':');
 
-        /// <summary>
-        /// Ensures the given tag is less than 129 characters, and does not start with invalid characters.
-        /// </summary>
-        /// <param name="imageTag"></param>
-        /// <returns></returns>
-        public static bool IsValidImageTag(string imageTag)
-        {
-            return imageTag.Length <= 128 && !imageTag.StartsWith('.') && !imageTag.StartsWith('-');
-        }
-
-        /// <summary>
-        /// Parse a fully qualified container name (e.g. https://mcr.microsoft.com/dotnet/runtime:6.0)
-        /// Note: Tag not required.
-        /// </summary>
-        /// <param name="fullyQualifiedContainerName"></param>
-        /// <param name="containerRegistry"></param>
-        /// <param name="containerName"></param>
-        /// <param name="containerTag"></param>
-        /// <returns>True if the parse was successful. When false is returned, all out vars are set to empty strings.</returns>
-        public static bool TryParseFullyQualifiedContainerName(string fullyQualifiedContainerName, out string containerRegistry, out string containerName, out string containerTag)
-        {
-            UriBuilder? uri = ContainerImageToUriBuilder(fullyQualifiedContainerName);
-
-            if (uri == null || uri.Uri.Segments.Length <= 1)
-            {
-                containerRegistry = "";
-                containerName = "";
-                containerTag = "";
-                return false;
-            }
-
-            // The first segment is the '/', create a string out of everything after.
-            string image = uri.Uri.Segments.Skip(1).Aggregate((str, next) => str + next);
-
-            // If the image has a ':', there's a tag we need to parse.
-            int indexOfColon = image.IndexOf(':');
-
-            containerRegistry = uri.Scheme + "://" + uri.Host;
-            containerName = indexOfColon == -1 ? image : image.Substring(0, indexOfColon);
-            containerTag = indexOfColon == -1 ? "" : image.Substring(indexOfColon + 1);
-            return true;
-        }
+        containerRegistry = uri.Scheme + "://" + uri.Host;
+        containerName = indexOfColon == -1 ? image : image.Substring(0, indexOfColon);
+        containerTag = indexOfColon == -1 ? "" : image.Substring(indexOfColon + 1);
+        return true;
     }
 }
--- Test.System.Containers/ContainerHelpersTests.cs
+++ Test.System.Containers/ContainerHelpersTests.cs
@@ -1,53 +1,55 @@
 using System.Containers;
 using System.Text;
 
-namespace Test.System.Containers
+namespace Test.System.Containers;
+
+[TestClass]
+public class ContainerHelpersTests
 {
-    [TestClass]
-    public class ContainerHelpersTests
+
+    [TestMethod]
+    [DataRow("https://mcr.microsoft.com/dotnet/runtime:6.0", true, "https://mcr.microsoft.com", "dotnet/runtime", "6.0")]
+    [DataRow("https://mcr.microsoft.com/dotnet/runtime", true, "https://mcr.microsoft.com", "dotnet/runtime", "")]
+    [DataRow("docker://mcr.microsoft.com/dotnet/runtime", true, "docker://mcr.microsoft.com", "dotnet/runtime", "")]
+    [DataRow("https://mcr.microsoft.com/", false, null, null, null)] // no image = nothing resolves
+    public void TryParseFullyQualifiedContainerName(string fullyQualifiedName, bool expectedReturn, string expectedRegistry, string expectedImage, string expectedTag)
     {
+        Assert.AreEqual(expectedReturn, ContainerHelpers.TryParseFullyQualifiedContainerName(fullyQualifiedName, out string? containerReg, out string? containerName, out string? containerTag));
+        Assert.AreEqual(expectedRegistry, containerReg);
+        Assert.AreEqual(expectedImage, containerName);
+        Assert.AreEqual(expectedTag, containerTag);
+    }
 
-        [TestMethod]
-        [DataRow("https://mcr.microsoft.com/dotnet/runtime:6.0", true, "https://mcr.microsoft.com", "dotnet/runtime", "6.0")]
-        [DataRow("https://mcr.microsoft.com/dotnet/runtime", true, "https://mcr.microsoft.com", "dotnet/runtime", "")]
-        [DataRow("https://mcr.microsoft.com/", false, "", "", "")] // no image = nothing resolves
-        [DataRow("docker://mcr.microsoft.com/dotnet/runtime", true, "docker://mcr.microsoft.com", "dotnet/runtime", "")]
-        public void TryParseFullyQualifiedContainerName(string fullyQualifiedName, bool expectedReturn, string expectedRegistry, string expectedImage, string expectedTag)
-        {
-            Assert.AreEqual(ContainerHelpers.TryParseFullyQualifiedContainerName(fullyQualifiedName, out string containerReg, out string containerName, out string containerTag), expectedReturn);
-            Assert.AreEqual(containerReg, expectedRegistry);
-            Assert.AreEqual(containerName, expectedImage);
-            Assert.AreEqual(containerTag, expectedTag);
-        }
+    [TestMethod]
+    [DataRow("dotnet/runtime", true)]
+    [DataRow("foo/bar", true)]
+    [DataRow("registry", true)]
+    [DataRow("-foo/bar", true)]
+    [DataRow(".foo/bar", true)]
+    [DataRow("foo/bar_", true)]
+    [DataRow("foo/bar.", true)]
+    [DataRow("_foo/bar", true)]
+    [DataRow("foo/bar-", true)]
+    [DataRow("-//-", false)]
+    public void IsValidImageName(string imageName, bool expectedReturn)
+    {
+        Assert.AreEqual(expectedReturn, ContainerHelpers.IsValidImageName(imageName));
+    }
 
-        [TestMethod]
-        [DataRow("dotnet/runtime", true)]
-        [DataRow("foo/bar", true)]
-        [DataRow("-foo/bar", false)]
-        [DataRow(".foo/bar", false)]
-        [DataRow("_foo/bar", false)]
-        [DataRow("foo/bar-", false)]
-        [DataRow("foo/bar.", false)]
-        [DataRow("foo/bar_", false)]
-        public void IsValidImageName(string imageName, bool expectedReturn)
-        {
-            Assert.AreEqual(ContainerHelpers.IsValidImageName(imageName), expectedReturn);
-        }
+    [TestMethod]
+    [DataRow("6.0", true)] // baseline
+    [DataRow("5.2+asd123", true)] // with commit hash
+    [DataRow(".6.0", true)] // starts with .
+    [DataRow("-6.0", true)] // starts with -
+    [DataRow("---", false)] // malformed
+    public void IsValidImageTag(string imageTag, bool expectedReturn)
+    {
+        Assert.AreEqual(expectedReturn, ContainerHelpers.IsValidImageTag(imageTag));
+    }
 
-        [TestMethod]
-        [DataRow("6.0", true)] // baseline
-        [DataRow("5.2+asd123", true)] // with commit hash
-        [DataRow(".6.0", false)] // starts with .
-        [DataRow("-6.0", false)] // starts with -
-        public void IsValidImageTag(string imageTag, bool expectedReturn)
-        {
-            Assert.AreEqual(ContainerHelpers.IsValidImageTag(imageTag), expectedReturn);
-        }
-
-        [TestMethod]
-        public void IsValidImageTag_InvalidLength()
-        {
-            Assert.AreEqual(ContainerHelpers.IsValidImageTag(new string(' ', 129)), false);
-        }
+    [TestMethod]
+    public void IsValidImageTag_InvalidLength()
+    {
+        Assert.AreEqual(false, ContainerHelpers.IsValidImageTag(new string('a', 129)));
     }
 }
