7b161abf7dc30987d806336fa2b734a37a834b67
Chet Husk <chusk3@gmail.com>
2022-08-08 15:43:50 -0500

    create the combined test targets during testing instead of adding a test hook to shipped targets

--- Test.Microsoft.NET.Build.Containers.Filesystem/TargetsTests.cs
+++ Test.Microsoft.NET.Build.Containers.Filesystem/TargetsTests.cs
@@ -7,15 +7,39 @@ namespace Test.Microsoft.NET.Build.Containers.Targets;
 [TestClass]
 public class TargetsTests
 {
+    private static string CombinedTargetsLocation;
+
+    private static string CombineFiles(string propsFile, string targetsFile)
+    {
+        var propsContent = System.IO.File.ReadAllLines(propsFile);
+        var targetsContent = System.IO.File.ReadAllLines(targetsFile);
+        var combinedContent = new List<string>();
+        combinedContent.AddRange(propsContent[..^1]);
+        combinedContent.AddRange(targetsContent[1..]);
+        var tempTargetLocation = Path.Combine(Path.GetTempPath(), "Containers", "ContainerTargets.targets");
+        System.IO.File.WriteAllLines(tempTargetLocation, combinedContent);
+        return tempTargetLocation;
+    }
+
     [ClassInitialize]
     public static void LocateMSBuild(TestContext ctx)
     {
         var instances = MSBuildLocator.QueryVisualStudioInstances(new() { DiscoveryTypes = DiscoveryType.DotNetSdk, WorkingDirectory = Environment.CurrentDirectory });
         MSBuildLocator.RegisterInstance(instances.First());
+
+        var targetsFile = CurrentFile.Relative("../Microsoft.NET.Build.Containers.Tasks/build/Microsoft.NET.Build.Containers.Tasks.targets");
+        var propsFile = Path.ChangeExtension(targetsFile, ".props");
+        CombinedTargetsLocation = CombineFiles(propsFile, targetsFile);
     }
 
-    private Project InitProject(Dictionary<string, string> bonusProps) {
-        var targetsFile = CurrentFile.Relative("../Microsoft.NET.Build.Containers.Tasks/build/Microsoft.NET.Build.Containers.Tasks.targets");
+    [ClassCleanup]
+    public static void Cleanup()
+    {
+        System.IO.File.Delete(CombinedTargetsLocation);
+    }
+
+    private Project InitProject(Dictionary<string, string> bonusProps)
+    {
         var props = new Dictionary<string, string>();
         // required parameters
         props["TargetFileName"] = "foo.dll";
@@ -28,15 +52,17 @@ public class TargetsTests
         props["CustomTasksAssembly"] = Path.GetFullPath(Path.Combine(".", "Microsoft.NET.Build.Containers.Tasks.dll"));
         props["_IsTest"] = "true";
 
-        var loggers = new List<ILogger> {
+        var loggers = new List<ILogger>
+        {
             // new Microsoft.Build.Logging.BinaryLogger() {CollectProjectImports = Microsoft.Build.Logging.BinaryLogger.ProjectImportsCollectionMode.Embed, Verbosity = LoggerVerbosity.Diagnostic, Parameters = "LogFile=blah.binlog" },
             // new Microsoft.Build.Logging.ConsoleLogger(LoggerVerbosity.Detailed)
         };
         var collection = new ProjectCollection(null, loggers, ToolsetDefinitionLocations.Default);
-        foreach (var kvp in bonusProps) {
+        foreach (var kvp in bonusProps)
+        {
             props[kvp.Key] = kvp.Value;
         }
-        return collection.LoadProject(targetsFile, props, null);
+        return collection.LoadProject(CombinedTargetsLocation, props, null);
     }
 
     [DataRow(true, "/app/foo.exe")]
@@ -44,13 +70,15 @@ public class TargetsTests
     [TestMethod]
     public void CanSetEntrypointArgsToUseAppHost(bool useAppHost, params string[] entrypointArgs)
     {
-        var project = InitProject(new() {
+        var project = InitProject(new()
+        {
             ["UseAppHost"] = useAppHost.ToString()
         });
         if (project.Build("ComputeContainerConfig"))
         {
             var computedEntrypointArgs = project.GetItems("ContainerEntrypoint").Select(i => i.EvaluatedInclude).ToArray();
-            foreach (var (First, Second) in entrypointArgs.Zip(computedEntrypointArgs)) {
+            foreach (var (First, Second) in entrypointArgs.Zip(computedEntrypointArgs))
+            {
                 Assert.AreEqual(First, Second);
             }
         }
