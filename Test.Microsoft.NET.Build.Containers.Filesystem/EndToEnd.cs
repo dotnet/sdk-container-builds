71610e34e9a12f37fb6642bab0b19e20f41aee38
Ben Villalobos <4691428+BenVillalobos@users.noreply.github.com>
2022-09-12 14:07:39 -0700

    Switch flag to MSBuildRuntimeType

--- Test.Microsoft.NET.Build.Containers.Filesystem/EndToEnd.cs
+++ Test.Microsoft.NET.Build.Containers.Filesystem/EndToEnd.cs
@@ -192,7 +192,7 @@ public class EndToEnd
                           $" /p:ContainerBaseImage={DockerRegistryManager.FullyQualifiedBaseImageDefault}" +
                           $" /p:ContainerRegistry=http://{DockerRegistryManager.LocalRegistry}" +
                           $" /p:ContainerImageName={NewImageName}" +
-                          $" /p:SomeVSFlag=true" +
+                          $" /p:MSBuildRuntimeType=Full" +
                           $" /p:Version=1.0";
 
         // Build & publish the project
