92d8b5de3c871878864ae2b018f17c7a6f9290a8
Ben Villalobos <4691428+BenVillalobos@users.noreply.github.com>
2022-09-16 09:26:54 -0700

    Remove NotNullWhenAttribute from ContainerHelpers

--- Microsoft.NET.Build.Containers/ContainerHelpers.cs
+++ Microsoft.NET.Build.Containers/ContainerHelpers.cs
@@ -16,26 +16,6 @@ public static class ContainerHelpers
     /// <remarks>Technically the period should be allowed as well, but due to inconsistent support between cloud providers we're removing it.</remarks>
     private static Regex imageNameCharacters = new Regex(@"[^a-z0-9_\-/]");
 
-#if !NET7_0_OR_GREATER
-    // Taken straight from dotnet/runtime for net472 visibility
-
-    /// <summary>
-    /// Specifies that when a method returns <see cref="ReturnValue"/>, the parameter will not be null even if the corresponding type allows it.
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
-    sealed class NotNullWhenAttribute : Attribute
-    {
-        /// <summary>Initializes the attribute with the specified return value condition.</summary>
-        /// <param name="returnValue">
-        /// The return value condition. If the method returns this value, the associated parameter will not be null.
-        /// </param>
-        public NotNullWhenAttribute(bool returnValue) => ReturnValue = returnValue;
-
-        /// <summary>Gets the return value condition.</summary>
-        public bool ReturnValue { get; }
-    }
-#endif
-
     /// <summary>
     /// Given some "fully qualified" image name (e.g. mcr.microsoft.com/dotnet/runtime), return
     /// a valid UriBuilder. This means appending 'https' if the URI is not absolute, otherwise UriBuilder will throw.
