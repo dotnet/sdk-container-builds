149d89ff485b82eaee22af91ef7e29e744f18966
Ben Villalobos <4691428+BenVillalobos@users.noreply.github.com>
2022-10-12 15:30:08 -0700

    Env var logic to ParseContainerProperties

--- Microsoft.NET.Build.Containers/ParseContainerProperties.cs
+++ Microsoft.NET.Build.Containers/ParseContainerProperties.cs
@@ -35,6 +35,11 @@ public class ParseContainerProperties : Microsoft.Build.Utilities.Task
     /// </summary>
     public string[] ContainerImageTags { get; set; }
 
+    /// <summary>
+    /// Container environment variables to set.
+    /// </summary>
+    public ITaskItem[] ContainerEnvironmentVariables { get; set; }
+
     [Output]
     public string ParsedContainerRegistry { get; private set; }
 
@@ -53,6 +58,9 @@ public class ParseContainerProperties : Microsoft.Build.Utilities.Task
     [Output]
     public string[] NewContainerTags { get; private set; }
 
+    [Output]
+    public ITaskItem[] NewContainerEnvironmentVariables { get; private set; }
+
     public ParseContainerProperties()
     {
         FullyQualifiedBaseImageName = "";
@@ -60,12 +68,14 @@ public class ParseContainerProperties : Microsoft.Build.Utilities.Task
         ContainerImageName = "";
         ContainerImageTag = "";
         ContainerImageTags = Array.Empty<string>();
+        ContainerEnvironmentVariables = Array.Empty<ITaskItem>();
         ParsedContainerRegistry = "";
         ParsedContainerImage = "";
         ParsedContainerTag = "";
         NewContainerRegistry = "";
         NewContainerImageName = "";
         NewContainerTags = Array.Empty<string>();
+        NewContainerEnvironmentVariables = Array.Empty<ITaskItem>();
     }
 
     private static bool TryValidateTags(string[] inputTags, out string[] validTags, out string[] invalidTags)
@@ -146,6 +156,8 @@ public class ParseContainerProperties : Microsoft.Build.Utilities.Task
             return !Log.HasLoggedErrors;
         }
 
+        ValidateEnvironmentVariables();
+
         if (FullyQualifiedBaseImageName.Contains(' ') && BuildEngine != null)
         {
             Log.LogWarning($"{nameof(FullyQualifiedBaseImageName)} had spaces in it, replacing with dashes.");
@@ -196,5 +208,26 @@ public class ParseContainerProperties : Microsoft.Build.Utilities.Task
         }
 
         return !Log.HasLoggedErrors;
+    }
+
+    public void ValidateEnvironmentVariables()
+    {
+        var filteredEnvVars = ContainerEnvironmentVariables.Where((x) => ContainerHelpers.IsValidEnvironmentVariable(x.ItemSpec)).ToArray<ITaskItem>();
+        var badEnvVars = ContainerEnvironmentVariables.Where((x) => !ContainerHelpers.IsValidEnvironmentVariable(x.ItemSpec));
+
+        foreach (var badEnvVar in badEnvVars)
+        {
+            if (BuildEngine != null)
+            {
+                Log.LogWarning($"{nameof(ContainerEnvironmentVariables)}: '{badEnvVar.ItemSpec}' was not a valid Environment Variable. Ignoring.");
+            }
+        }
+
+        NewContainerEnvironmentVariables = new ITaskItem[filteredEnvVars.Length];
+
+        for (int i = 0; i < filteredEnvVars.Length; i++)
+        {
+            NewContainerEnvironmentVariables[i] = filteredEnvVars[i];
+        }
     }
 }
