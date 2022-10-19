2dc1d532f566ccee5c865db87f9356ec274c0ff9
Ben Villalobos <4691428+BenVillalobos@users.noreply.github.com>
2022-10-19 13:56:28 -0700

    Create Exceptions.cs and add ContainerHttpException

---
+++ Microsoft.NET.Build.Containers/Exceptions.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.Serialization;
+
+namespace Microsoft.NET.Build.Containers;
+
+public class DockerLoadException : Exception
+{
+    public DockerLoadException()
+    {
+    }
+
+    public DockerLoadException(string? message) : base(message)
+    {
+    }
+
+    public DockerLoadException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    protected DockerLoadException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
+
+public class ContainerHttpException : Exception
+{
+    public string json;
+    public ContainerHttpException(string message) : base(message)
+    {
+        json = "";
+    }
+}
