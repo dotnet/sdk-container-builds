105baebc6816e96128cb7fb35258fb51b6e09dfc
Ben Villalobos <4691428+BenVillalobos@users.noreply.github.com>
2022-10-21 11:13:46 -0700

    Capitalize consts

--- Microsoft.NET.Build.Containers/ContainerHttpException.cs
+++ Microsoft.NET.Build.Containers/ContainerHttpException.cs
@@ -1,11 +1,11 @@
 namespace Microsoft.NET.Build.Containers;
 public class ContainerHttpException : Exception
 {
-    private const string errorPrefix = "Containerize: error CONTAINER004:";
+    private const string ErrorPrefix = "Containerize: error CONTAINER004:";
     string? jsonResponse;
     string? uri;
     public ContainerHttpException(string message, string? targetUri, string? jsonResp)
-            : base($"{errorPrefix} {message}\nURI: {targetUri ?? "None."}\nJson Response: {jsonResp ?? "None."}")
+            : base($"{ErrorPrefix} {message}\nURI: {targetUri ?? "None."}\nJson Response: {jsonResp ?? "None."}")
     {
         jsonResponse = jsonResp;
         uri = targetUri;
