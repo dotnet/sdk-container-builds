173d806581a7a8a76b1e69fada0f8248ef98ad6d
Norm Johanson <normj@amazon.com>
2023-01-05 09:41:56 -0800

    Fix issues pushing images to Amazon ECR

--- Test.Microsoft.NET.Build.Containers.Filesystem/RegistryTests.cs
+++ Test.Microsoft.NET.Build.Containers.Filesystem/RegistryTests.cs
@@ -19,4 +19,19 @@ public class RegistryTests
 
         Assert.IsNotNull(downloadedImage);
     }
+
+    [DataRow("public.ecr.aws", true)]
+    [DataRow("123412341234.dkr.ecr.us-west-2.amazonaws.com", true)]
+    [DataRow("123412341234.dkr.ecr-fips.us-west-2.amazonaws.com", true)]
+    [DataRow("notvalid.dkr.ecr.us-west-2.amazonaws.com", false)]
+    [DataRow("1111.dkr.ecr.us-west-2.amazonaws.com", false)]
+    [DataRow("mcr.microsoft.com", false)]
+    [DataRow("localhost", false)]
+    [DataRow("hub", false)]
+    [TestMethod]
+    public void CheckIfAmazonECR(string registryName, bool isECR)
+    {
+        Registry registry = new Registry(ContainerHelpers.TryExpandRegistryToUri(registryName));
+        Assert.AreEqual(isECR, registry.IsAmazonECRRegistry);
+    }
 }
