5eedc02babda7ccff65f6495e9e8c66b76998a7c
Vlada Shubina <vshubina@microsoft.com>
2023-02-07 15:48:40 +0100

    fixed file headers

--- Test.Microsoft.NET.Build.Containers.Filesystem/EndToEnd.cs
+++ Test.Microsoft.NET.Build.Containers.Filesystem/EndToEnd.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.DotNet.CommandUtils;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.DotNet.CommandUtils;
 using Microsoft.NET.Build.Containers;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
--- Test.Microsoft.NET.Build.Containers.Filesystem/TransientTestFolder.cs
+++ Test.Microsoft.NET.Build.Containers.Filesystem/TransientTestFolder.cs
@@ -1,4 +1,7 @@
-﻿using static System.IO.Path;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using static System.IO.Path;
 
 namespace Test.Microsoft.NET.Build.Containers.Filesystem;
 
